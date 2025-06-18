1e025a6d1fad26d1eaf75f2031f383b66fdc11d0
Hiron Das <hcdas.09@gmail.com>
2025-06-18 12:51:23 -0400

    work in progress

---
+++ src/domain.rs
@@ -0,0 +1,1 @@
+pub struct SubscriberName<'a>(pub &'a str);
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,12 +2,15 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use unicode_segmentation::UnicodeSegmentation;
+use crate::domain::SubscriberName;
 
 #[derive(serde::Deserialize)]
 pub struct FromData {
     name: String,
     email: String,
 }
+
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
@@ -18,6 +21,11 @@ pub struct FromData {
     )
 )]
 pub async fn subscribe(form: web::Form<FromData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let subscriber_name = SubscriberName(&form.name);
+
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
@@ -45,4 +53,16 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FromData) -> Result<(), sql
     .execute(pool)
     .await?;
     Ok(())
+}
+
+
+pub fn is_valid_name(s: &str)-> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_chracters = ['/', '(',')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_chracters.contains(&g));
+
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
