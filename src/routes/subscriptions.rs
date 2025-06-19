0212aede794340ee16a9955b004cce71819229e3
Hiron Das <hcdas.09@gmail.com>
2025-06-19 18:05:12 +0600

    in progress

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,16 +1,15 @@
+use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use uuid::Uuid;
 use unicode_segmentation::UnicodeSegmentation;
-use crate::domain::SubscriberName;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FromData {
     name: String,
     email: String,
 }
-
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
@@ -21,48 +20,42 @@ pub struct FromData {
     )
 )]
 pub async fn subscribe(form: web::Form<FromData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let subscriber_name = SubscriberName(&form.name);
+    // let subscriber_name = SubscriberName(&form.name);
 
-    if !is_valid_name(&form.name) {
-        return HttpResponse::BadRequest().finish();
-    }
-    match insert_subscriber(&pool, &form).await {
+    let new_subscriber = NewSubscriber {
+        name: SubscriberName::parse(form.0.name),
+        email: form.0.email,
+    };
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
+        Err(e) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FromData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, name, email, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.name,
-        form.email,
+        new_subscriber.name.as_ref(),
+        new_subscriber.email,
         Utc::now()
     )
     .execute(pool)
-    .await?;
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
     Ok(())
-}
-
-
-pub fn is_valid_name(s: &str)-> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-
-    let forbidden_chracters = ['/', '(',')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidden_characters = s.chars().any(|g| forbidden_chracters.contains(&g));
-
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
