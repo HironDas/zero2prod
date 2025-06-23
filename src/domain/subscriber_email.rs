388de8488368d04bdaca24d0a4c2b932ce8a9014
Hiron Das <hcdas.09@gmail.com>
2025-06-23 18:06:03 +0600

    in progress

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,1 +1,55 @@
 //! src/domain/subscriber_email.rs
+use validator::ValidateEmail;
+
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail{
+    pub fn parse(s: String) -> Result<Self, String>{
+        if <String as ValidateEmail>::validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("Invalid subscriber email: {}", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+
+#[cfg(test)]
+mod tests{
+    use claim::{assert_err, assert_ok};
+    use fake::{faker::internet::raw::SafeEmail, locales::EN, Fake};
+
+    use super::*;
+
+    #[test]
+    fn valid_emails_are_parsed_successfully(){
+        let email = SafeEmail(EN).fake();
+        assert_ok!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn empty_string_is_rejected(){
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected(){
+        let email = "testemail.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected(){
+        let email = "@testemail.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    } 
+
+}   
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
@@ -8,6 +8,16 @@ use uuid::Uuid;
 pub struct FromData {
     name: String,
     email: String,
+}
+
+impl TryFrom<FromData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FromData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(NewSubscriber { name, email })
+    }
 }
 
 #[tracing::instrument(
@@ -20,18 +30,15 @@ pub struct FromData {
 )]
 pub async fn subscribe(form: web::Form<FromData>, pool: web::Data<PgPool>) -> HttpResponse {
     // let subscriber_name = SubscriberName(&form.name);
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(e) => {
-            tracing::error!("Invalid subscriber name: {}", e);
+            tracing::error!("Failed to parse new subscriber: {}", e);
             return HttpResponse::BadRequest().finish();
         }
     };
 
-    let new_subscriber = NewSubscriber {
-        name,
-        email: form.0.email,
-    };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_e) => HttpResponse::InternalServerError().finish(),
@@ -53,7 +60,7 @@ pub async fn insert_subscriber(
         "#,
         Uuid::new_v4(),
         new_subscriber.name.as_ref(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         Utc::now()
     )
     .execute(pool)
