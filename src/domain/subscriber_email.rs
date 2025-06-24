06e20c59e0f99b5bac53ea0fc01a2da02c8b1cb6
Hiron Das <hcdas.09@gmail.com>
2025-06-24 18:04:18 +0600

    done chapter 6

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -4,8 +4,8 @@ use validator::ValidateEmail;
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
-impl SubscriberEmail{
-    pub fn parse(s: String) -> Result<Self, String>{
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<Self, String> {
         if <String as ValidateEmail>::validate_email(&s) {
             Ok(Self(s))
         } else {
@@ -20,36 +20,62 @@ impl AsRef<str> for SubscriberEmail {
     }
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
+    use super::*;
     use claim::{assert_err, assert_ok};
     use fake::{faker::internet::raw::SafeEmail, locales::EN, Fake};
-
-    use super::*;
+    use proptest::prelude::*;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
     #[test]
-    fn valid_emails_are_parsed_successfully(){
+    fn valid_emails_are_parsed_successfully() {
         let email = SafeEmail(EN).fake();
         assert_ok!(SubscriberEmail::parse(email));
     }
 
+    #[derive(Debug, Clone)]
+    struct SubscriberEmailFixture(pub String);
+
+    // prop_compose! {
+    //     fn valid_email()
+    //     (mut rng in any::<u64>().prop_map(|seed| StdRng::seed_from_u64(seed))) 
+    //     (email in SafeEmail(EN).fake_with_rng(&mut rng)) -> SubscriberEmailFixture {
+    //         SubscriberEmailFixture(email)
+    //     }
+    // }
+
+    fn safe_email_strategy()-> impl Strategy<Value = SubscriberEmailFixture> {
+        any::<u64>().prop_map(|seed| {
+            let mut rng = StdRng::seed_from_u64(seed);
+            let email = SafeEmail(EN).fake_with_rng(&mut rng);
+            SubscriberEmailFixture(email)
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn valid_emails_are_parsed_successfully_proptest(email in safe_email_strategy()) {
+            assert_ok!(SubscriberEmail::parse(email.0));
+        }
+    }
+
     #[test]
-    fn empty_string_is_rejected(){
+    fn empty_string_is_rejected() {
         let email = "".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
 
     #[test]
-    fn email_missing_at_symbol_is_rejected(){
+    fn email_missing_at_symbol_is_rejected() {
         let email = "testemail.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
 
     #[test]
-    fn email_missing_subject_is_rejected(){
+    fn email_missing_subject_is_rejected() {
         let email = "@testemail.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
-    } 
-
-}   
+    }
+}
