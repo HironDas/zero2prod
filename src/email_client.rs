caffe8138d78bf6e26b21913732f6a2e3b7dfaf9
Hiron Das <hcdas.09@gmail.com>
2025-06-25 18:13:18 +0600

    in progess of chapter 7

---
+++ src/email_client.rs
@@ -0,0 +1,30 @@
+use std::ops::Sub;
+
+use reqwest::Client;
+
+use crate::domain::SubscriberEmail;
+
+pub struct EmailClient {
+    http_client: reqwest::Client,
+    base_url: String,
+    sender: SubscriberEmail,
+}
+
+impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        todo!("Implement email sending logic here");
+    }
+}
