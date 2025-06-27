9cf11293c79354bf833704f5a685c35154b6179b
Hiron Das <hcdas.09@gmail.com>
2025-06-26 21:10:33 -0400

    done

--- src/email_client.rs
+++ src/email_client.rs
@@ -25,16 +25,45 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!("Implement email sending logic here");
+        Ok(())
     }
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
+
+    use fake::{
+        faker::{
+            internet::raw::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+        locales::EN,
+        Fake,
+    };
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
     use super::*;
-   #[tokio::test]
-   async fn send_email_fires_a_request_to_base_url(){
-    todo!("Implement a test that sends an email and checks the request to the base URL");
-   }
+    
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail(EN).fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail(EN).fake()).unwrap();
+
+        let subject: String = Sentence(1..2).fake();
+        let content = Paragraph(1..10).fake::<String>();
+
+        // Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+    }
 }
