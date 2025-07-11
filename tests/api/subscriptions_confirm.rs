6142b4956cd5f6b56c8482acdd9d55087745f1b7
Hiron Das <hcdas.09@gmail.com>
2025-07-11 18:09:13 +0600

    in progress

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -12,3 +12,15 @@ async  fn confirmations_without_token_are_rejected_with_a_400() {
     // Assert
     assert_eq!(400, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called(){
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=Hiron%20Das&email=hcdas.09%40gmail.com";
+
+    app.post_subscriptions(body.into())
+        .await;
+
+
+}
